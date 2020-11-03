cfd8a27a41dbafed993da996ed24fecb227ace8f
MYATHETCHAL-PC\Mya Thet Chal <scm.myathetchal@gmail.com>
2020-11-03 21:07:22 +0630

    environment setup commit

---
+++ resources/views/includes/head.blade.php
@@ -0,0 +1,9 @@
+<meta charset="utf-8">
+<meta name="description" content="">
+<meta name="author" content="Scotch">
+
+<title></title>
+
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script>
