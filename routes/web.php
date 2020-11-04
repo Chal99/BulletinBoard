ac94ca6365c7e5b14fefa7528aa8f8b938937ff9
MYATHETCHAL-PC\Mya Thet Chal <scm.myathetchal@gmail.com>
2020-11-04 11:26:53 +0630

    Post List Developed

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,13 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('layouts.default');
+});
+
+Route::get('/postlists', function () {
+    return view('posts.index');
+});
+
+Route::get('/userlists', function () {
+    return view('users.index');
 });
